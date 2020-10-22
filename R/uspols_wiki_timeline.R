e70a6a1e4965dcc013e2116dd0c8a13ab08dab7b
Jason Timm <jaetimm@gmail.com>
2020-10-22 11:07:39 -0600

    re

--- R/uspols_wiki_timeline.R
+++ R/uspols_wiki_timeline.R
@@ -7,42 +7,42 @@
 #' @rdname uspols_wiki_timeline
 
 uspols_wiki_timeline <- function() {
-  
+
   qs <- c('_Q1', '_Q2', '_Q3', '_Q4')
   ys <- c(2017:2020)
   allqs <- do.call(paste0, expand.grid(ys, qs))
-  
-  
-  timeline <- lapply(1:length(allqs), function(x) {  
-    
+
+
+  timeline <- lapply(1:length(allqs), function(x) {
+
     url1 <- paste0('https://en.wikipedia.org/wiki/Timeline_of_the_Donald_Trump_presidency_(',
                    allqs[x], ')')
-    
+
     ###
-    tq_list <- xml2::read_html(url1) 
-    tq_list1 <- rvest::html_nodes(tq_list, "table")      
+    tq_list <- xml2::read_html(url1)
+    tq_list1 <- rvest::html_nodes(tq_list, "table")
     tq_list2 <- rvest::html_table(tq_list1, fill = TRUE)
-    
+
     new <- lapply(1:length(tq_list2), function(z) {
-      
+
       if ( all(c('Date', 'Events') %in% colnames(tq_list2[[z]]))  ) {
-        
+
         tq_list2[[z]][, c('Date', 'Events')]
-        
-      } else{ data.frame(Date = '', Events = '') 
-      }   
+
+      } else{ data.frame(Date = '', Events = '')
+      }
     })
-    
+
     out <- data.table::rbindlist(Filter(function(i) nrow(i) >= 3, new))
     out$quarter <- allqs[x]
     out <- subset(out, !grepl('\\[edit', Date))
     out$Events <- gsub('\\[[0-9]+\\]', '', out$Events)
-    out$Date <-  as.Date(paste0(gsub('_..', '', out$quarter),  
-                                gsub('^.*,', '', out$Date)), 
+    out$Date <-  as.Date(paste0(gsub('_..', '', out$quarter),
+                                gsub('^.*,', '', out$Date)),
                          "%Y %B %d")
     out[, c('quarter', 'Date', 'Events')]
   })
-  
+
   y <- data.table::rbindlist(timeline)
-  y[order(Date)]
+  y[order(y$Date)]
 }
